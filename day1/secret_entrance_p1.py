19dcf1639ee711f07d307241c177565e472c1711
Ethan-Villanueva <e.villanueva.cs@outlook.com>
2025-12-01 10:47:37 +0000

    day 1 part 1

---
+++ day1/secret_entrance_p1.py
@@ -0,0 +1,21 @@
+NUM_OF_DIALS = 100
+point = 50
+password = 0
+
+with open("day1_input.txt", "r") as f:
+    for line in f:
+        direction = line[0]
+        clicks = int(line[1:])
+
+        if line[0] == 'L':
+            point -= clicks
+            while point < 0:
+                point += NUM_OF_DIALS
+        else:
+            point += clicks
+        
+        point %= NUM_OF_DIALS
+        if point == 0:
+            password += 1
+
+print(password)
