f20b4952c668431293c6d39f2c67a25bcd6d7c5e
Ethan-Villanueva <e.villanueva.cs@outlook.com>
2025-12-29 17:47:14 +0000

    Completed day 2 part 1, and cleaned day 1

--- day1/secret_entrance_p1.py
+++ day1/secret_entrance_p1.py
@@ -2,18 +2,43 @@ NUM_OF_DIALS = 100
 point = 50
 password = 0
 
-with open("day1_input.txt", "r") as f:
-    for line in f:
-        direction = line[0]
-        clicks = int(line[1:])
+def part1():
+    global point,password
+    with open("day1_input.txt", "r") as f:
+        for line in f:
+            direction = line[0]
+            clicks = int(line[1:])
 
-        if line[0] == 'L':
-            point -= clicks
-        else:
-            point += clicks
-        
-        point %= NUM_OF_DIALS
-        if point == 0:
-            password += 1
+            if line[0] == 'L':
+                point -= clicks
+            else:
+                point += clicks
+            
+            point %= NUM_OF_DIALS
+            if point == 0:
+                password += 1
 
-print(password)
+    print(password)
+
+def part2():
+    global point,password
+    with open("day1_input.txt", "r") as f:
+        for line in f:
+            direction = line[0]
+            clicks = int(line[1:])
+
+            full_rots = clicks // NUM_OF_DIALS
+            password += full_rots
+            clicks %= NUM_OF_DIALS
+            if direction == 'L':
+                if clicks >= point:
+                    password += 1
+                point -= clicks
+            else:
+                if clicks >= NUM_OF_DIALS - point:
+                    password += 1
+                point += clicks
+
+            point %= NUM_OF_DIALS
+
+    print(password)
---
+++ day2/gift_shop.py
@@ -0,0 +1,21 @@
+invalid_IDs = []
+
+def part1():
+    global invalid_IDs
+    with open("day2_input.txt", "r") as f:
+        ranges = f.read().split(',')
+        for ID_range in ranges:
+            start, end = ID_range.split('-')
+            for i in range(int(start), int(end)+1):
+                digits = str(i)
+
+                if len(digits) % 2 == 1: continue
+                else:
+                    half_way = len(digits)//2
+                    first_half = digits[0:half_way]
+                    second_half = digits[half_way:]
+
+                    if first_half == second_half:
+                        invalid_IDs.append(int(digits))
+        
+        print(sum(invalid_IDs))
